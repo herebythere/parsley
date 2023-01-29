3119779177b151892ea3ea92ef925621356498a0
brian taylor vann <brian.t.vann@gmail.com>
2023-01-29 03:06:39 -0500

    dont export routes

--- deno/v0.1/mod.ts
+++ deno/v0.1/mod.ts
@@ -2,7 +2,6 @@ export type { Position, Vector } from "./type_flyweight/text_vector.ts";
 export type {
   BuilderInterface,
   BuildStep,
-  Routes,
 } from "./type_flyweight/parse.ts";
 
 export { parse } from "./parse/parse.ts";
