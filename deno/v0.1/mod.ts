f54606aaec011f6e3204c9cce0458b951cfce625
brian taylor vann <brian.t.vann@gmail.com>
2023-01-29 03:11:36 -0500

    format

--- deno/v0.1/mod.ts
+++ deno/v0.1/mod.ts
@@ -1,7 +1,4 @@
 export type { Position, Vector } from "./type_flyweight/text_vector.ts";
-export type {
-  BuilderInterface,
-  BuildStep,
-} from "./type_flyweight/parse.ts";
+export type { BuilderInterface, BuildStep } from "./type_flyweight/parse.ts";
 
 export { parse } from "./parse/parse.ts";
