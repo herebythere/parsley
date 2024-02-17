92009e6581cce0f7005728780c1a25f6dc853d8d
taylor <brian.t.vann@gmail.com>
2024-02-16 17:00:52 -0800

    add injection type

--- rust/v0.1/parsley/src/parse.rs
+++ rust/v0.1/parsley/src/parse.rs
@@ -54,6 +54,8 @@ fn get_injection_type(build_step: &str) -> Option<&str> {
 // if INJECTION FOUND
 //   add injection_step
 
+// keep previously found kind at injection
+
 pub fn parse_str<T: Builder>(
 	mut builder: T,
 	template: &str,
@@ -64,39 +66,45 @@ pub fn parse_str<T: Builder>(
 	
 	let mut prev_inj_kind = INITIAL;
 	let mut curr_inj_kind = INITIAL;
+	let mut inj_found_kind = INITIAL;
 	let mut inj_origin = 0;
 	
 	for (index, glyph) in template.char_indices() {
-		println!("{}{}", &index, &glyph);
 		prev_kind = curr_kind;
 		curr_kind = routes::route(&glyph, prev_kind);
 		
 		prev_inj_kind = curr_inj_kind;
 		curr_inj_kind = injection_routes::route(&glyph, prev_inj_kind);
+
 		
-		
+		// if injection found
 		if prev_inj_kind == INJECTION_CONFIRMED {
 			// new start, bring both origins to cursor
 			// add node step
 			if origin < inj_origin {
 					builder = builder.add_step(NodeStep{
-					kind: prev_kind.to_string(),
+					kind: inj_found_kind.to_string(),
 					vector: Vector{origin: origin, target: inj_origin},
 				});
 			}
 			
 			// add injection step
+			let injection_kind = match get_injection_type(inj_found_kind) {
+				Some(inj) => inj,
+				None => "INJECTION_UNKNOWN_TYPE",
+			};
+
+			println!("{} {}", inj_found_kind, injection_kind);
 			builder = builder.add_step(NodeStep{
-				kind: INJECTION.to_string(),
+				kind: injection_kind.to_string(),
 				vector: Vector{origin: inj_origin, target: index},
 			});
-			
+
 			origin = inj_origin;
-			
 			prev_kind = curr_kind;
 		}
-		
 
+		// if delta found 
 		if prev_kind != curr_kind {
 			builder = builder.add_step(NodeStep{
 				kind: prev_kind.to_string(),
@@ -106,8 +114,10 @@ pub fn parse_str<T: Builder>(
 			origin = index;
 		}
 		
+		// possible injection
 		if curr_inj_kind == INJECTION_FOUND {
-			inj_origin = index
+			inj_origin = index;
+			inj_found_kind = prev_kind;
 		}
 	}
 
@@ -123,16 +133,20 @@ pub fn parse_str<T: Builder>(
 			});
 		}
 
+		let injection_kind = match get_injection_type(curr_kind) {
+			Some(inj) => inj,
+			None => "INJECTION_UNKNOWN_TYPE",
+		};
+
 		// add injection step
 		return builder.add_step(NodeStep{
-			kind: INJECTION.to_string(),
+			kind: injection_kind.to_string(),
 			vector: Vector{origin: inj_origin, target: template.len()},
 		});
 	}
 	
-	
 	builder.add_step(NodeStep{
-		kind: prev_kind.to_string(),
+		kind: curr_kind.to_string(),
 		vector: Vector{origin: origin, target: template.len()},
 	})
 }
